fc556f0ebbdfdc0c143cf8fe73dab52bfd3cbdef
CraneMeDaddyLLC <38012764+CraneMeDaddyLLC@users.noreply.github.com>
2018-04-09 13:21:40 -0400

    Update search.js

--- father/search.js
+++ father/search.js
@@ -4,6 +4,9 @@ function search() {
   this.query = document.getElementById("searchbar").value;
   
   if (this.query != "" && this.query != null)
+    if (this.query.toUpperCase() == "JOE FLEMMING")
+        window.location = "https://en.wikipedia.org/wiki/Black_Hebrew_Israelites";
+
     window.location = "https://www.google.com/search?q=" + this.query;
 }
 
