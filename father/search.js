cbb21299ae74236533cbe131a8e9dbb621433f39
CraneMeDaddyLLC <38012764+CraneMeDaddyLLC@users.noreply.github.com>
2018-04-09 14:11:52 -0400

    Update search.js

--- father/search.js
+++ father/search.js
@@ -4,8 +4,12 @@ function search() {
   this.query = document.getElementById("searchbar").value;
   
   if (this.query != "" && this.query != null)
-    if (this.query.toUpperCase() == "JOE FLEMMING" || this.query.toUpperCase() == "JOEFLEMMING")
+    if (this.query.toUpperCase() == "JOE FLEMMING" || this.query.toUpperCase() == "JOEFLEMMING" || this.query.toUpperCase() == "JOE")
         window.location = "https://en.wikipedia.org/wiki/Black_Hebrew_Israelites";
+    else if (this.query.toUpperCase() == "MATTHEW NEMETH" || this.query.toUpperCase() == "MATTHEWNEMETH" || this.query.toUpperCase() == "NEMETH")
+        window.location = "https://en.wikipedia.org/wiki/Rhodesia";
+    else if (this.query.toUpperCase() == "DAVID ACEVEDO" || this.query.toUpperCase() == "DAVIDACEVEDO" || this.query.toUpperCase() == "DAVE")
+        window.location = "";
     else
       window.location = "https://www.google.com/search?q=" + this.query;
 }
