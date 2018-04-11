ff7a8a7595267ee5fb50a8745f586022a9f62c1d
CraneMeDaddyLLC <38012764+CraneMeDaddyLLC@users.noreply.github.com>
2018-04-11 14:30:25 -0400

    Update search.js

--- father/search.js
+++ father/search.js
@@ -6,6 +6,8 @@ function search() {
   if (this.query != "" && this.query != null)
     if (this.query.toUpperCase() == "JOE FLEMMING" || this.query.toUpperCase() == "JOEFLEMMING" || this.query.toUpperCase() == "JOE")
         window.location = "https://en.wikipedia.org/wiki/Black_Hebrew_Israelites";
+    else if (this.query.toUpperCase() == "AVOCADO")
+        window.location = "http://www.jewgle.com/imates/avocado.jpg";
     else if (this.query.toUpperCase() == "MATTHEW NEMETH" || this.query.toUpperCase() == "MATTHEWNEMETH" || this.query.toUpperCase() == "NEMETH")
         window.location = "https://en.wikipedia.org/wiki/Rhodesia";
     else if (this.query.toUpperCase() == "DAVID ACEVEDO" || this.query.toUpperCase() == "DAVIDACEVEDO" || this.query.toUpperCase() == "DAVE")
